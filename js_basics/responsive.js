032f138fdfd28b93407f9fd6e310448711ced38a
Afsal <thenameisafsalahamad@gmail.com>
2024-10-01 21:02:44 +0530

    figured out how to print user input to the ocnsole

--- js_basics/responsive.js
+++ js_basics/responsive.js
@@ -1,6 +1,4 @@
-function s(){
-let v = document.getElementById("name").value;
-console.log(v);
-let fullNamme = prompt("Enter your name:");
-console.log(fullNamme);
+function s() {
+  let v = document.getElementById("name").value;
+  console.log(v);
 }
