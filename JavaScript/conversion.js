b0db256e9216067fafc6dbadcba590944e729080
Afsal <thenameisafsalahamad@gmail.com>
2024-10-07 19:35:52 +0530

    some more conversions

--- JavaScript/conversion.js
+++ JavaScript/conversion.js
@@ -3,3 +3,9 @@ console.log(typeof Number(val)); // converted the vlaue
 let val2 = "33abc";
 let converted = Number(val2); // will result in NaN
 console.log(typeof converted, converted);
+
+let test = null;
+console.log(Number(test)); // null to number results in 0
+
+let test2 = undefined;
+console.log(Number(test2)); // undefined to number results in NaN
