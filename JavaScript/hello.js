dc8e786f24f1e74f0ad5c86de9aed21d4992857a
Afsal <thenameisafsalahamad@gmail.com>
2024-10-07 19:27:24 +0530

    data types covered

--- JavaScript/hello.js
+++ JavaScript/hello.js
@@ -4,4 +4,5 @@ let isLoggedn = true; // boolean
 console.table([myName, age]);
 let something = Number("999"); // type cast
 console.log(something);
-console.log(typeof age);
+console.log(typeof null); // null is of type object, while undefined is of type undefined
+console.log(Number.MAX_VALUE);
