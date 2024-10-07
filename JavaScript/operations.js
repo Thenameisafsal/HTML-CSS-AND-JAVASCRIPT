8c067b1e9c7a2cccb47a32a1f0446da5f12e8334
Afsal <thenameisafsalahamad@gmail.com>
2024-10-07 19:57:31 +0530

    part 2 of conversions

--- JavaScript/operations.js
+++ JavaScript/operations.js
@@ -5,8 +5,8 @@ console.log(1 + 2 + "2");
 console.log("1" + 1 + 2 + "2");
 
 console.log(true);
-console.log(+true);
-console.log(-true);
-console.log("hello" + true);
-console.log(1 + false);
-console.log(1 - false);
+console.log(+true); // 1
+console.log(-true); // -1
+console.log("hello" + true); // hellotrue
+console.log(1 + false); //1
+console.log(1 - false); //1
