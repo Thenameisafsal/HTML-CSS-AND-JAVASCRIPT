25e1a02d341da48f5381b4b62ec407972854fa68
Afsal <thenameisafsalahamad@gmail.com>
2024-10-07 19:57:02 +0530

    tricky conversions

--- JavaScript/conversion.js
+++ JavaScript/conversion.js
@@ -9,3 +9,13 @@ console.log(Number(test)); // null to number results in 0
 
 let test2 = undefined;
 console.log(Number(test2)); // undefined to number results in NaN
+
+// convert boolean to number -> 1 for true and 0 for false
+let test3 = true;
+console.log(Number(test3));
+// integer to boolean
+console.log(Boolean(1));
+console.log(Boolean(2));
+
+// "" -> false
+// "something" -> true
---
+++ JavaScript/operations.js
@@ -0,0 +1,12 @@
+let s = "hello";
+let t = " world";
+console.log(s + t);
+console.log(1 + 2 + "2");
+console.log("1" + 1 + 2 + "2");
+
+console.log(true);
+console.log(+true);
+console.log(-true);
+console.log("hello" + true);
+console.log(1 + false);
+console.log(1 - false);
