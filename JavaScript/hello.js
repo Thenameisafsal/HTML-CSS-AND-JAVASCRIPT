ded98344943e09acc80eb7f06d8b345935be4b8c
Afsal <thenameisafsalahamad@gmail.com>
2024-10-07 19:04:41 +0530

    data types

--- JavaScript/hello.js
+++ JavaScript/hello.js
@@ -1,3 +1,7 @@
-let myName = "afsal";
-let age = 19;
+let myName = "afsal"; // string
+let age = 19; // int - number
+let isLoggedn = true; // boolean
 console.table([myName, age]);
+let something = Number("999"); // type cast
+console.log(something);
+console.log(typeof age);
