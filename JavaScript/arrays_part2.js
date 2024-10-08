fbf5da4976bc29716609ec5466f2724cbafe7703
Afsal <thenameisafsalahamad@gmail.com>
2024-10-08 20:19:16 +0530

    array part 2

---
+++ JavaScript/arrays_part2.js
@@ -0,0 +1,7 @@
+marvel = ["wolverine", "ironman", "spiderman"];
+dc = ["batman", "superman", "flash"];
+console.log(marvel + dc);
+// alternatively we can use concat() to concatenate the arrays
+console.log(marvel.concat(dc));
+marvel.push(dc);
+console.log(marvel);
