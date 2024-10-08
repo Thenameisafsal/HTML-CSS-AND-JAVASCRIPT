c3b513ed7513b977fbd92f5b711213468155b3b3
Afsal <thenameisafsalahamad@gmail.com>
2024-10-08 15:28:06 +0530

    strings

---
+++ JavaScript/strings.js
@@ -0,0 +1,5 @@
+// string interpolation - use ``
+
+let name = "Afsal";
+let age = 20;
+console.log(`hello, i am ${name} and i am ${age} years old`); // advantage : use string methods within {} interpolation and more readable
