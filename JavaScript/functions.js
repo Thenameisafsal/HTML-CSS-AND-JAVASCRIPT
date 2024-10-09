dc6dbc5af3f7f0f0152ba708aa0e18b05034adbd
Afsal <thenameisafsalahamad@gmail.com>
2024-10-09 12:03:59 +0530

    this keyword

---
+++ JavaScript/functions.js
@@ -0,0 +1,10 @@
+if (true) {
+  let username = "afsal";
+  function some() {
+    console.log(this);
+    // this will print engine data in functions
+    // you won't need this keyword in this context
+  }
+}
+
+some();
