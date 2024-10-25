1073fdbfc7fe9308470201fb47dcee6245c305f0
Afsal <thenameisafsalahamad@gmail.com>
2024-10-25 19:43:38 +0530

    args to promises

--- promises/one.js
+++ promises/one.js
@@ -8,3 +8,25 @@ const promiseOne = new Promise(function (resolve, reject) {
 promiseOne.then(function () {
   console.log("the promise was excuted");
 });
+
+// if you do not want to store the promise:
+new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    console.log("in the promise now");
+  }, 1000);
+  resolve();
+}).then(function () {
+  console.log("promise has been settled");
+});
+
+// to pass arguments to the promise
+const promiseThree = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    console.log("let's pass some args to the promise");
+    // you pass args through the resolve function
+    resolve({ name: "afsal", course: "aiml" });
+  }, 1000);
+});
+promiseThree.then(function (user) {
+  console.log(user);
+});
