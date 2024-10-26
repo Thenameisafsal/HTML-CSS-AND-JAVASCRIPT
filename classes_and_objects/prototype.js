27ea8623a777f53b587da9b45ef7b9f56d9b9524
Afsal <thenameisafsalahamad@gmail.com>
2024-10-26 21:38:39 +0530

    prototype magic

--- classes_and_objects/prototype.js
+++ classes_and_objects/prototype.js
@@ -6,3 +6,11 @@ String.prototype.truelength = function () {
 // will work with any string now - no need to define for each and every string
 
 console.log(myName.truelength());
+
+// similarly for any given data type, let's specify a method using prototype
+Object.prototype.afsal = function () {
+  console.log("hi i am afsal brotha!");
+};
+
+const d = 5;
+d.afsal();
