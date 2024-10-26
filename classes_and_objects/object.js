4cf78aad86443ec14cd06c467f41ae7c788cd98d
Afsal <thenameisafsalahamad@gmail.com>
2024-10-26 21:18:03 +0530

    objects and properties of prototype

--- classes_and_objects/object.js
+++ classes_and_objects/object.js
@@ -9,3 +9,16 @@ multiply.power = 2;
 console.log(multiply(5));
 console.log(multiply.power);
 console.log(multiply.prototype);
+
+function createUser(user, age) {
+  this.user = user;
+  this.age = age;
+}
+// inject your function using prototype
+createUser.prototype.increment = function () {
+  this.age++;
+};
+
+const obj = new createUser("afsal", 19);
+obj.increment();
+console.log(obj);
