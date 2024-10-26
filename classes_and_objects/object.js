1ec2892c31917d4340d3439f843229133f3f69a2
Afsal <thenameisafsalahamad@gmail.com>
2024-10-26 21:24:10 +0530

    function and prototype along with importance of this

--- classes_and_objects/object.js
+++ classes_and_objects/object.js
@@ -18,7 +18,12 @@ function createUser(user, age) {
 createUser.prototype.increment = function () {
   this.age++;
 };
-
+// without new keyword you won't be able to access the injected method
 const obj = new createUser("afsal", 19);
 obj.increment();
 console.log(obj);
+
+// // example:
+// const obj1 = createUser("afsal", 19);
+// obj1.increment(); // you will not be able to read the property as obj1.increment() will evaluate to undefined
+// console.log(obj1);
