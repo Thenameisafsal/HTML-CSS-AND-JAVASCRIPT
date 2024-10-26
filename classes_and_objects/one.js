0ed5518b745112b6624bc1b3d78c1aedbfea0967
Afsal <thenameisafsalahamad@gmail.com>
2024-10-26 13:57:59 +0530

    fetch

---
+++ classes_and_objects/one.js
@@ -0,0 +1,13 @@
+class o {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  what() {
+    console.log(this.name, this.age);
+  }
+}
+
+let guy = new o("afsal", 23);
+guy.what();
