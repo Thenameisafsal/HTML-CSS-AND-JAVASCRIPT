a125ea6a67a7c1709edb8da4377145cfd09941e0
Afsal <thenameisafsalahamad@gmail.com>
2024-10-29 19:17:39 +0530

    starting react+vite

--- vite/basics/src/App.jsx
+++ vite/basics/src/App.jsx
@@ -1,28 +1,15 @@
 import { useState } from 'react'
-
+import Youtube from './youtube'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-        </a>
-        <a href="https://react.dev" target="_blank">
-        </a>
+<p>vite</p>       
+<p>vite</p>      
+<Youtube/>  {/* imported from another jsx */}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ vite/basics/src/youtube.jsx
@@ -0,0 +1,7 @@
+function Youtube(){
+    return (
+        <h1>youtube</h1>
+    )
+}
+
+export default Youtube; /*export to another file*/
