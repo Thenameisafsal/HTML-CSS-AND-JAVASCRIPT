678bdff43c0d5ecd25576afa5ba98dedea7f3d49
Afsal <thenameisafsalahamad@gmail.com>
2024-11-04 11:59:19 +0530

    react manual

---
+++ learn_react/index.js
@@ -0,0 +1,4 @@
+function Component() {
+  return <h1> hey i am learning react!</h1>;
+}
+ReactDOM.render(<Component />, document.getElementById("root"));
--- vite/basics2/color-changer/src/App.jsx
+++ vite/basics2/color-changer/src/App.jsx
@@ -8,9 +8,9 @@ function App() {
       <div className='w-full h-screen duration-200' style={{backgroundColor:color}}>
         <div className='fixed flex flew-wrap justify-center bottom-12 inset-x-0 px-2'>
           <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
-            <button className='outline-none px-4 py-1 rounded-full text-black shadow-lg' onClick={() => {setter("red")}}>RED</button>
-            <button className='outline-none px-4 py-1 rounded-full text-black shadow-lg' onClick={() => {setter("green")}}>GREEN</button>
-            <button className='outline-none px-4 py-1 rounded-full text-black shadow-lg' onClick={() => {setter("blue")}}>BLUE</button>
+            <button className='outline-none px-4 py-1 rounded-full text-black shadow-lg' onClick={() => {setter("red")}} style={{backgroundColor:'red'}}>RED</button>
+            <button className='outline-none px-4 py-1 rounded-full text-black shadow-lg' onClick={() => {setter("green")}} style={{backgroundColor:'green'}}>GREEN</button>
+            <button className='outline-none px-4 py-1 rounded-full text-black shadow-lg' onClick={() => {setter("blue")}} style={{backgroundColor:'blue'}}>BLUE</button>
           </div>
         </div>
       </div>
