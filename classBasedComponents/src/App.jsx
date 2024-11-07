481aab1f4b4d4e02924ffb54458e31bbabc2abb0
Afsal <thenameisafsalahamad@gmail.com>
2024-11-08 00:11:52 +0530

    class based components using react.component and render method

---
+++ classBasedComponents/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+  class Component extends React.Component{
+    render(){
+      return (
+        <h1>You're viewing a component that is based on a class</h1>
+      )
+    }
+}
+
+export default Component
