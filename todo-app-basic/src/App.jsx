ae08b098c861cea89e008e6cc752d4710f65cd18
Afsal <thenameisafsalahamad@gmail.com>
2024-11-07 13:07:49 +0530

    styling the components

---
+++ todo-app-basic/src/App.jsx
@@ -0,0 +1,14 @@
+const App = function(){
+  return (
+    <form>
+      <label>task1</label>
+      <input type="checkbox"></input>
+      <label>task2</label>
+      <input type="checkbox"></input>
+      <label>task3</label>
+      <input type="checkbox"></input>
+    </form>
+  )
+}
+
+export default App
