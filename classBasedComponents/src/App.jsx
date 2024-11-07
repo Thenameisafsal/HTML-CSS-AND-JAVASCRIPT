cd4e8c89498d5f630738613d2443ecf52930e462
Afsal <thenameisafsalahamad@gmail.com>
2024-11-08 01:41:22 +0530

    access props in class based components

--- classBasedComponents/src/App.jsx
+++ classBasedComponents/src/App.jsx
@@ -4,6 +4,7 @@ import React from 'react'
     render(){
       return (
         <h1>You're viewing a component that is based on a class</h1>
+        // use {this.props.something} syntax to access the props
       )
     }
 }
