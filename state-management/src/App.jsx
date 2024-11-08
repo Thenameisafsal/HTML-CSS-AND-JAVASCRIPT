df52a43e179a89e5b1099de01c9b8fcd3f0ff473
Afsal <thenameisafsalahamad@gmail.com>
2024-11-08 08:34:49 +0530

    state management basics

---
+++ state-management/src/App.jsx
@@ -0,0 +1,12 @@
+import {React,useState} from 'react'
+const App = function(){
+  const [value,setValue] = useState(0)
+  return (
+    <div>
+      <h1>Current Value: {value}</h1>
+      <button onClick={() => value<3 ? setValue(value+1) : console.log("upper limit has been reached, please reduce the value")}>Press me to increase the value</button>
+      <button onClick = {() => {value>0 ? setValue(value-1) : console.log("lower limit reached, please increase the value")}}>Press me to decrease the value</button>
+    </div>
+  )
+}
+export default App
