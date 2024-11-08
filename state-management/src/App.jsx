38acd43e1dbf0a880024db809965314d3101cda4
Afsal <thenameisafsalahamad@gmail.com>
2024-11-08 10:31:19 +0530

    useState

--- state-management/src/App.jsx
+++ state-management/src/App.jsx
@@ -3,7 +3,7 @@ const App = function(){
   const [value,setValue] = useState(0)
   return (
     <div>
-      <h1 id="displayval">Current Value: {value}</h1>
+      <h1 id="displayval" style={{fontWeight:700}}>Current Value: {value}</h1>
       <button onClick={() => value<3 ? setValue(value+1) : console.log("upper limit has been reached, please reduce the value")}>Press me to increase the value</button>
       <button onClick = {() => {value>0 ? setValue(value-1) : console.log("lower limit reached, please increase the value")}}>Press me to decrease the value</button>
     </div>
